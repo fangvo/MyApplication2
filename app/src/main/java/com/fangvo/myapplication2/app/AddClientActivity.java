f6df146088d046505320608950d9d87b750c1133
fangvo <fangvo@gmail.com>
2015-05-31 10:38:33 +0300

    Some coments

--- app/src/main/java/com/fangvo/myapplication2/app/AddClientActivity.java
+++ app/src/main/java/com/fangvo/myapplication2/app/AddClientActivity.java
@@ -16,11 +16,16 @@ import java.util.Map;
 
 public class AddClientActivity extends Activity {
 
+
+	// иничиализация активности
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_client);
 
+		// поиск кнопок по ид и устоновка оброботчика
+		
         Button btn8 = (Button)findViewById(R.id.button8);
         btn8.setOnClickListener(onClickListener);
 
@@ -28,6 +33,8 @@ public class AddClientActivity extends Activity {
         btn9.setOnClickListener(onClickListener);
     }
 
+	// оброботчик нажатий на кнопки
+	
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(final View v) {
@@ -44,8 +51,12 @@ public class AddClientActivity extends Activity {
         }
     };
 
+	// сбор и отправка даных в дб
+	
     private void SendToDB(){
 
+	// переменые
+	
         String cname;
         String adres;
         Long inn;
@@ -56,6 +67,9 @@ public class AddClientActivity extends Activity {
         Long bik;
         String dir;
 
+		
+		// поиск тесбоксов по id
+		
         EditText editTextCName = (EditText)findViewById(R.id.editText2);
         EditText editTextAdres = (EditText)findViewById(R.id.editText3);
         EditText editTextInn = (EditText)findViewById(R.id.editText4);
@@ -66,11 +80,15 @@ public class AddClientActivity extends Activity {
         EditText editTextBIK = (EditText)findViewById(R.id.editText9);
         EditText editTextdir = (EditText)findViewById(R.id.editText10);
 
+		// проверка заполености всех полей
+		
         if (editTextCName.getEditableText().toString().equals("")||editTextAdres.getEditableText().toString().equals("")||editTextInn.getEditableText().toString().equals("")||editTextKpp.getEditableText().toString().equals("")||editTextBank.getEditableText().toString().equals("")||editTextRS.getEditableText().toString().equals("")||editTextKS.getEditableText().toString().equals("")||editTextBIK.getEditableText().toString().equals("")||editTextdir.getEditableText().toString().equals("")){
             Toast.makeText(AddClientActivity.this,"Необходимо заполнить все поля",Toast.LENGTH_SHORT).show();
             return;
         }
 
+		// присвоение значеий текстбоксов переменым
+		
         cname = editTextCName.getEditableText().toString();
         adres = editTextAdres.getEditableText().toString();
         inn = Long.parseLong(editTextInn.getEditableText().toString());
@@ -81,8 +99,7 @@ public class AddClientActivity extends Activity {
         bik = Long.parseLong(editTextBIK.getEditableText().toString());
         dir = editTextdir.getEditableText().toString();
 
-
-
+		// создание карты значений и добовление ее в лист
 
         List<Map<Integer,Object>> list  = new ArrayList<Map<Integer,Object>>();
         Map<Integer,Object> dict = new HashMap<Integer, Object>();
@@ -98,9 +115,13 @@ public class AddClientActivity extends Activity {
         dict.put(10, dir);
         list.add(dict);
 
+		// добовление имени клиента в список имен клиентов
         MyData.clientsName.add(cname);
 
+		// запуск асинхроной отвпавки даных в дб
         new AsyncUpdate(list,getApplicationContext()).execute("INSERT into Clients values(?,?,?,?,?,?,?,?,?,?)");
+		
+		// закрытие активности
         finish();
 
     }
